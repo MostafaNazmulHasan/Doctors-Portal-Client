ca74ac9ad25c223d456a5a58c4529f4ae7ee89e4
MostafaNazmulHasan <mostafanazmulhasan@gmail.com>
2021-09-06 09:34:26 +0600

    doctors portal

---
+++ src/components/Home/BusinessInfo/BusinessInfo.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { faClock, faMapMarker, faPhone } from '@fortawesome/free-solid-svg-icons'
+import InfoCard from '../InfoCard/InfoCard';
+const infosData = [
+    {
+        title: 'Opening Hours',
+        description: 'we are open 7 days',
+        icon: faClock,
+        background: 'primary'
+
+    },
+    {
+        title: 'Visit Our Location',
+        description: 'Brooklyn, NY 10003, USA',
+        icon: faMapMarker,
+        background: 'dark'
+
+    },
+    {
+        title: 'Call us now',
+        description: '+123465',
+        icon: faPhone,
+        background: 'primary'
+
+    }
+]
+const BusinessInfo = () => {
+    return (
+        <section className='d-flex justify-content-center'>
+            <div className='w-75 row'>
+                {
+                    infosData.map((info) => <InfoCard info={info}></InfoCard>)
+                }
+            </div>
+        </section>
+    );
+};
+
+export default BusinessInfo;
